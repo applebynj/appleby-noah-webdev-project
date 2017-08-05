7243240d3a960c1f09e4767adb51dd86b351f214
Noah Appleby <appleby.n@husky.neu.edu>
2017-08-04 23:05:02 -0400

    implement services using web service calls

--- public/project/services/place.service.client.js
+++ public/project/services/place.service.client.js
@@ -12,11 +12,19 @@
         return api;
 
         function findPlaceById(placeId) {
-            //TODO
+            var url = 'https://maps.googleapis.com/maps/api/place/details/' +
+                'json?placeid=' + placeId +
+                '&key=' + 'AIzaSyDTKeMGhvu0jabXMmqlQK1_kue5NIrgXbk';
+            //TODO: destroy this key and set as var in heroku instead
+            return $http.get(url);
         }
 
         function findPlaceByTextSearch(searchText) {
-            //TODO
+            var url = 'https://maps.googleapis.com/maps/api/place/textsearch/' +
+                    'json?query=' + searchText +
+                    '&key=' + 'AIzaSyDTKeMGhvu0jabXMmqlQK1_kue5NIrgXbk';
+            //TODO: destroy this key and set as var in heroku instead
+            return $http.get(url);
         }
     }
 })();
