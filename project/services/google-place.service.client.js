36bbd9d68c0195eea473627c0c4d9330f32e08ec
Noah Appleby <appleby.n@husky.neu.edu>
2017-08-10 07:31:29 -0400

    update calls to api with proxy query

--- project/services/google-place.service.client.js
+++ project/services/google-place.service.client.js
@@ -2,31 +2,68 @@ var app = require("../../express");
 var https = require("https");
 var querystring = require('querystring');
 
-var places = [
-
-];
-
 app.get("/api/google/place/:placeId", findPlaceById);
 app.get("/api/google/place/search", findPlaceByTextSearch);
 
+var apiKey = 'AIzaSyDTKeMGhvu0jabXMmqlQK1_kue5NIrgXbk'; //TODO: replace with environment variable
+var googleMapsHost = 'https://maps.googleapis.com';
+var googlePlacesBaseUrl = '/maps/api/place/';
 
-function findPlaceById(placeId) {
-/*  TODO: Need to make these calls a different way than on the front end (below is the call for reference
-
-    var url = 'https://maps.googleapis.com/maps/api/place/details/' +
-        'json?placeid=' + placeId +
-        '&key=' + 'AIzaSyDTKeMGhvu0jabXMmqlQK1_kue5NIrgXbk';
-    //TODO: destroy this key and set as var in heroku instead
-    return $http.get(url);*/
-
+function findPlaceById(req, res) {
+    var placeId = req.params.placeId;
+    googlePlaceQueryById(placeId)
+        .then(function(response){
+            res.json(response);
+        }, function(err) {
+            res.sendStatus(404).send(err);
+        });
 }
 
-function findPlaceByTextSearch(searchText) {
-    /*  TODO: Need to make these calls a different way than on the front end (below is the call for reference
+function findPlaceByTextSearch(req, res) {
+    var searchText = req.body.searchText;
+    googlePlaceSearchQuery(searchText)
+        .then(function(response){
+            res.json(response);
+        }, function(err) {
+            res.sendStatus(404).send(err);
+        });
+}
 
-     /*     var url = 'https://maps.googleapis.com/maps/api/place/textsearch/' +
-        'json?query=' + searchText +
-        '&key=' + 'AIzaSyDTKeMGhvu0jabXMmqlQK1_kue5NIrgXbk';
-     //TODO: destroy this key and set as var in heroku instead
-     return $http.get(url);*/
+function googlePlaceQueryById(placeId) {
+    return $http.get({
+        host: googleMapsHost,
+        path: googlePlacesBaseUrl + 'details/json?placeid=' + placeId,
+        headers: {
+            "Accept": "application/json",
+            "app_key": apiKey
+        }
+    }, digestResponse(response));
+}
+
+function googlePlaceSearchQuery(searchText) {
+    return $http.get({
+        host: googleMapsHost,
+        path: googlePlacesBaseUrl + 'details/json?textSearch=' + searchText,
+        headers: {
+            "Accept": "application/json",
+            "app_key": apiKey
+        }
+    }, digestResponse(response));
+}
+
+function digestResponse(response){
+    var deferred = q.defer();
+
+    var body = '';
+    response.on('data', function(d) {
+        body += d;
+    });
+    response.on('end', function() {
+        try {
+            body = JSON.parse(body);
+            deffered.resolve(body);
+        } catch(e) {
+            deferred.reject({error: e});
+        }
+    })
 }
