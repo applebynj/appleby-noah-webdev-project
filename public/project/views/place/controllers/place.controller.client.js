842459fe8a0228b508d39418a3690229b647e57a
Noah Appleby <appleby.n@husky.neu.edu>
2017-08-11 21:10:10 -0400

    copy place pages from poc to new dir

---
+++ public/project/views/place/controllers/place.controller.client.js
@@ -0,0 +1,21 @@
+(function() {
+    angular
+        .module("WbdvProject")
+        .controller("PlaceController", PlaceController)
+
+    function PlaceController($routeParams, GooglePlaceService) {
+        var model = this;
+
+        model.placeId = $routeParams["pid"];
+
+        function init() {
+            GooglePlaceService
+                .findPlaceById(model.placeId)
+                .then(function(response) {
+                    model.place = response.data.result;
+
+                });
+        }
+        init();
+    }
+})();
