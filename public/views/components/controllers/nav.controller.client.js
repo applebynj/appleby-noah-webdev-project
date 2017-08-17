a539693107b1cacd89faabb11c437082118dd683
Noah Appleby <appleby.n@husky.neu.edu>
2017-08-17 17:41:29 -0400

    add search results to drop down off of nav

--- public/views/components/controllers/nav.controller.client.js
+++ public/views/components/controllers/nav.controller.client.js
@@ -8,19 +8,20 @@
 
         model.findPlaceByTextSearch = findPlaceByTextSearch;
 
-        function init() { console.log(model.user); }
+        function init() {}
         init();
 
         function findPlaceByTextSearch() {
             //Remove all spaces TODO: remove other escape chars as it's a url param
-            var searchText = model.whatSearchText + " " + model.whereSearchText;
-            searchText = searchText.replace(/ /g, '+');
-            GooglePlaceService
-                .findPlaceByTextSearch(searchText)
-                .then(function(response) {
-                    model.results = response.data.results;
-                })
-
+            if(model.whatSearchText && model.whereSearchText) {
+                var searchText = model.whatSearchText + " " + model.whereSearchText;
+                searchText = searchText.replace(/ /g, '+');
+                GooglePlaceService
+                    .findPlaceByTextSearch(searchText)
+                    .then(function(response) {
+                        model.results = response.data.results;
+                    })
+            }
         }
     }
 })();
