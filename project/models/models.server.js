1f1ecbfb7da11f201aa7baede6e89f8279589392
Noah Appleby <appleby.n@husky.neu.edu>
2017-08-17 23:57:08 -0400

    fix mongodb url to use heroku

--- project/models/models.server.js
+++ project/models/models.server.js
@@ -4,8 +4,7 @@ var connectionString = 'mongodb://127.0.0.1:27017/project'; // for local
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += process.env.MONGODB_PRE + process.env.MONGODB_URI;
+    connectionString = process.env.MONGODB_URI;
 }
 
 var mongoose = require("mongoose");
