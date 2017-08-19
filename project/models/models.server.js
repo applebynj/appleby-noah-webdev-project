0db6f7872c0b1b8d5518aa61af67d2be0b8ae637
Noah Appleby <appleby.n@husky.neu.edu>
2017-08-18 22:06:48 -0400

    ignore environment varible files

--- project/models/models.server.js
+++ project/models/models.server.js
@@ -1,4 +1,5 @@
 var q = require('q');
+require('dotenv').config();
 
 var connectionString = 'mongodb://127.0.0.1:27017/project'; // for local
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
